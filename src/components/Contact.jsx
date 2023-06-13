8065fc9884dfaa17ff62806e960ead196397e807
Eh1z <godsentehiz11@gmail.com>
2023-06-13 23:42:16 +0100

    fixed fonts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
       <h1 className='head_text font-russo text_gradient pt-16 pb-5'>Join Us</h1>
       <p className=' font-medium font-outfit text-xl w-[40%] text-center pb-10'>We have a fast growing community on Telegram,  join our channel below for more info and faster updates on the state of the project.  </p>
       
-      <div className='flex flex-col items-center justify-center'>
-        <img className='w-16 mx-2 my-2' src={images.telegram} alt="telegram" />
+      <div className='flex flex-col items-center justify-center hover:scale-110 transition-all duration-700 animate-pulse'>
+        <img className='w-16 my-2 translate-x-[-5px]' src={images.telegram} alt="telegram" />
         <span className='font-russo text-xl text_gradient'>Telegram</span>
       </div>
     </div>
